dea25924ca5edb259b3dc81beed6007ec44946db
Kirsty Adair <kirstyaadair@gmail.com>
2019-02-27 18:44:31 +0000

    WE HAVE A KETCHUP GUN

--- Assets/Scripts/Weapon Scripts/KetchupBeamScript.cs
+++ Assets/Scripts/Weapon Scripts/KetchupBeamScript.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class KetchupBeamScript : MonoBehaviour
 {
     public GameObject ketchupPrefab;
-    Vector3 direction1 = new Vector3(0, 0, 1);
-    Vector3 direction2 = new Vector3(0.1f, 0, 1);
-    Vector3 direction3 = new Vector3(-0.1f, 0, 1);
-    bool fireAgain = true;
-    float timeUntilDisappear = 1.5f;
+    Vector3 direction = new Vector3(0, 0, 1);
 
     // Start is called before the first frame update
     void Start()
@@ -18,23 +14,14 @@ public class KetchupBeamScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetButton("Fire1"))
         {
-            if (fireAgain)
-            {
-                GameObject ketchupDir1 = (GameObject)Instantiate(ketchupPrefab, transform.position, Quaternion.identity);
-                ketchupDir1.GetComponent<Rigidbody>().AddForce(direction1, ForceMode.Impulse);
-                StartCoroutine(StartRapidFire());
-            }
+            GameObject ketchup = (GameObject)Instantiate(ketchupPrefab, transform.position, Quaternion.identity);
+            ketchup.AddComponent<KetchupScript>();
+            ketchup.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
         }
     }
-
-    IEnumerator StartRapidFire()
-    {
-        fireAgain = false;
-        yield return new WaitForSeconds(0.1f);
-        fireAgain = true;
-    }
+    
 }
---
+++ Assets/Scripts/Weapon Scripts/KetchupScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KetchupScript : MonoBehaviour
+{
+    float timeToDestroy = 3.0f;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToDestroy -= Time.deltaTime;
+        transform.localScale += new Vector3(0.2f, 0f, 0.1f);
+
+        if (timeToDestroy <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Ground")
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionY & RigidbodyConstraints.FreezePositionZ & RigidbodyConstraints.FreezePositionY;
+            transform.localScale = new Vector3(20, 0.1f, 10);
+        }
+    }
+}
