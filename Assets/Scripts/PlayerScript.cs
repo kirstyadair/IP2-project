6adec875cc950c5efc0f0ecfafe060f7e24e5d48
Jishaxe <josh@joshbe.me>
2019-03-02 15:57:38 +0000

    refactor some player code

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,40 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum ChefType
+{
+    FAT, CRAZY, THIN
+}
+
 public class PlayerScript : MonoBehaviour
 {
     public GameObject reticle;
-    public GameObject knifePrefab;
-    public Vector3 reticlePos;
+
+    [Tooltip("Hitpoints before player dies")]
+    int numOfHits = 5;
+
+    [Tooltip("Speed the player moves at")]
     public float moveSpeed;
+
+    [Tooltip("Is this player AI-controlled?")]
+    public bool isAIControlled;
+
+    [Tooltip("Type of chef")]
+    public ChefType chefType;
+
+    // These will be moved to separate script soon
     public float fireCooldown;
-    public float fireTimeout = 0;
-    float movementCooldown = 0;
-    ParticleSystem blood;
+    public GameObject knifePrefab;
+    float fireTimeout = 0;
+    /////////////////////////////////
+
     Rigidbody rb;
     GameData gameData;
     LineRenderer lineRenderer;
-    bool isAbleToFire = false;
-    bool tempAI;
-    int numOfHits = 5;
+
+    [HideInInspector]
+    public Vector3 reticlePos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        blood = GetComponent<ParticleSystem>();
         gameData = GameObject.Find("GameData").GetComponent<GameData>();
         rb = GetComponent<Rigidbody>();
         lineRenderer = GetComponent<LineRenderer>();
         gameData.OnStateChange += OnStateChange;
-
-        if (gameObject.tag == "TempAI")
-        {
-            tempAI = true;
-        }
-        else
-        {
-            tempAI = false;
-        }
     }
 
 
@@ -46,7 +54,7 @@ public class PlayerScript : MonoBehaviour
         fireTimeout -= Time.deltaTime;
         
         // human player reticle
-        if (!tempAI)
+        if (!isAIControlled)
         {
             // raycast to place reticle
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -58,45 +66,13 @@ public class PlayerScript : MonoBehaviour
             lineRenderer.enabled = true;
             lineRenderer.SetPosition(0, transform.position);
             lineRenderer.SetPosition(1, reticlePos);
-            //rb.simulated = true;
-        }
-        else // ai reticle
-        {
-            reticlePos = new Vector3(0, 0, 0);
+            reticle.transform.position = reticlePos;
 
-            // If line is not set to the same position then it extends the size of the bounding rect and transform.position is all wack
-            lineRenderer.SetPosition(0, transform.position);
-            lineRenderer.SetPosition(1, transform.position);
-            lineRenderer.enabled = false;
-            //rb.simulated = false;
-        }
-
-
-        if (!tempAI) reticle.transform.position = reticlePos;
-
-        movementCooldown -= Time.deltaTime;
-
-        //if (movementCooldown > 0) return;
-
-        if (!tempAI)
-        {
             Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer"), 0, Input.GetAxis("VerticalPlayer"));
             rb.AddForce(direction, ForceMode.Impulse);
-        }
 
-        //rb.velocity = direction * moveSpeed;
-
-        Vector3 minScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
-        Vector3 maxScreenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height));
-
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, minScreenBounds.x + 1, maxScreenBounds.x - 1), Mathf.Clamp(transform.position.y, minScreenBounds.y + 1, maxScreenBounds.y - 1), transform.position.z);
-
-        if (!tempAI)
-        {
             if (Input.GetMouseButton(0) == true && fireTimeout <= 0)
             {
-               // reticle.GetComponent<Animator>().SetTrigger("pulse");
-
                 Vector3 bulletDirection = reticlePos - transform.position;
                 bulletDirection.Normalize();
 
@@ -105,16 +81,25 @@ public class PlayerScript : MonoBehaviour
                 fireTimeout = fireCooldown;
             }
         }
+        else // if we are AI controlled
+        {
+            reticlePos = new Vector3(0, 0, 0);
+
+            // If line is not set to the same position then it extends the size of the bounding rect and transform.position is all wack
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position);
+            lineRenderer.enabled = false;
+        }
     }
 
+    // Colliding with a zombie, take damage!
     public void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Zombie")
         {
+            // Push the player away from the zombie
             Vector3 normal = collision.GetContact(0).normal;
             rb.AddForce(normal * 5, ForceMode.Impulse);
-            //blood.Emit(20);
-            movementCooldown = 0.5f;
 
             numOfHits--;
 
@@ -131,7 +116,6 @@ public class PlayerScript : MonoBehaviour
     {
         if (fireTimeout > 0) return;
         if (gameData.state == GameState.PREP) return; // Don't shoot whilst we're preparing
-        if (!isAbleToFire) return; // Don't fire if firing is disabled
         GameObject knife = (GameObject)Instantiate(knifePrefab, transform.position, Quaternion.identity);
         
         knife.GetComponent<Rigidbody>().AddForce(direction, ForceMode.Impulse);
@@ -141,36 +125,7 @@ public class PlayerScript : MonoBehaviour
 
     public void OnStateChange(GameState oldState, GameState newState)
     {
-        if (newState == GameState.PLAY) isAbleToFire = true;
+
     }
 
-    public void IncreaseMoveSpeed()
-    {
-        moveSpeed++;
-    }
-
-    public void DecreaseMoveSpeed()
-    {
-        moveSpeed--;
-    }
-
-    public void IncreaseDrag()
-    {
-        rb.drag += 0.5f;
-    }
-
-    public void DecreaseDrag()
-    {
-        rb.drag -= 0.5f;
-    }
-
-    public void IncreaseFiringCooldown()
-    {
-        fireCooldown += 0.1f;
-    }
-
-    public void DecreaseFiringCooldown()
-    {
-        fireCooldown -= 0.1f;
-    }
 }
