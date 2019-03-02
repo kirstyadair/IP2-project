69ce3ac28345bb009214b50a6919a90f7f6f93ad
Kirsty Adair <kirstyaadair@gmail.com>
2019-03-02 01:56:24 +0000

    did stuff

--- Assets/Scripts/Weapon Scripts/FPScript.cs
+++ Assets/Scripts/Weapon Scripts/FPScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FPScript : MonoBehaviour
 {
+    public GameObject reticule;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,5 +35,6 @@ public class FPScript : MonoBehaviour
             transform.Translate(Vector3.right * 10 * Time.deltaTime);
         }
 
+        reticule.transform.position = Input.mousePosition;
     }
 }
