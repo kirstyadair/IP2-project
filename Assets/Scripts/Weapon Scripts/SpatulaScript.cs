1d6704111c572f4c11b4e54d03a7160051dc7a57
Kirsty Adair <kirstyaadair@gmail.com>
2019-03-03 19:17:31 +0000

    adding the spatula

--- Assets/Scripts/Weapon Scripts/SpatulaScript.cs
+++ Assets/Scripts/Weapon Scripts/SpatulaScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class SpatulaScript : MonoBehaviour
 {
+    float hitCountdown = 2.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,6 +15,21 @@ public class SpatulaScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        hitCountdown -= Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && hitCountdown <= 0)
+        {
+            Collider[] hitEnemies = Physics.OverlapSphere(transform.position, 0.5f);
+            Debug.Log("Number of hit enemies: " + hitEnemies.Length);
+            foreach (Collider sushi in hitEnemies)
+            {
+                if (sushi.tag == "Zombie")
+                {
+                    sushi.gameObject.GetComponent<ZombieScript>().Hit();
+                }
+            }
+            hitCountdown = 2.0f;
+        }
         
     }
 }
