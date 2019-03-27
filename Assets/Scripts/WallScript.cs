0916717050609bd02c0884157b168a6b3170f61a
Kirsty Adair <kirstyaadair@gmail.com>
2019-03-27 17:34:33 +0000

    updated wallscript

--- Assets/Scripts/WallScript.cs
+++ Assets/Scripts/WallScript.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class WallScript : MonoBehaviour
 {
+    // this script is here because ketchupScript isn't around anymore - this would be in OnTriggerEnter of that script
+    // it only exists to stop ketchup from travelling through walls and inflicting damage
+
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "KetchupBlob")
         {
-            Debug.Log("Wall collision");
             Destroy(other.gameObject);
         }
     }
