ae77531e0950bf985629d2299e3cbd4fe474858e
Kirsty Adair <kirstyaadair@gmail.com>
2019-04-08 01:38:27 +0100

    made a script

--- Assets/Scripts/CursorScript.cs
+++ Assets/Scripts/CursorScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using InControl;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +8,17 @@ public class CursorScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // for the love of god, remember to attach this to the fucking gameobject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        InputDevice currentInput = InputManager.ActiveDevice;
+
+        if (currentInput.LeftStick.Left)
+        {
+            Debug.Log("LEEEEEEEFT");
+        }
     }
 }
